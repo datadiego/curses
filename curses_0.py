2abbb4b9e65b7d809226246fdbb3ef544f61df6c
Juan Diego <juandiegomariscal@gmail.com>
2022-07-11 13:39:29 +0200

    feat: 👍añadidos atributos

--- curses_0.py
+++ curses_0.py
@@ -3,10 +3,12 @@ from curses import A_BOLD, A_UNDERLINE, wrapper
 
 def main(stdscr):
     stdscr.clear()
-    stdscr.addstr("AAAA")
+    stdscr.addstr(1, 1, "AAAA")
     stdscr.addstr("BBBB", A_BOLD)
-    stdscr.addstr(20, 0, "HELLO")
-    stdscr.addstr(20, 20, "KTHXBYE", A_UNDERLINE)
+    stdscr.addstr(5, 1, "HELLO", curses.A_BLINK)
+    stdscr.addstr(6,1, "REVERSED", curses.A_REVERSE)
+    stdscr.addstr(7,1, "STANDOUT", curses.A_STANDOUT)
+    stdscr.addstr(8, 6, "KTHXBYE", A_UNDERLINE)
     stdscr.refresh()
     stdscr.getch()
 
