95613d647ec11eead5791e131768a592a1aaf84b
Juan Diego <juandiegomariscal@gmail.com>
2022-07-11 13:54:32 +0200

    feat: 👍añadidos colores

--- curses_0.py
+++ curses_0.py
@@ -2,6 +2,8 @@ import curses
 from curses import A_BOLD, A_UNDERLINE, wrapper
 
 def main(stdscr):
+    curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_YELLOW)
+    BLUE_YELLOW = curses.color_pair(1)
     stdscr.clear()
     stdscr.addstr(1, 1, "AAAA")
     stdscr.addstr("BBBB", A_BOLD)
@@ -9,6 +11,7 @@ def main(stdscr):
     stdscr.addstr(6,1, "REVERSED", curses.A_REVERSE)
     stdscr.addstr(7,1, "STANDOUT", curses.A_STANDOUT)
     stdscr.addstr(8, 6, "KTHXBYE", A_UNDERLINE)
+    stdscr.addstr(10, 10, "COLOR TEST", BLUE_YELLOW)
     stdscr.refresh()
     stdscr.getch()
 
